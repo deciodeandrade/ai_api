8ffab89172f07b77d77cbf535a4fbbdd14513e71
Décio <deciocavalcantedeandrade@gmail.com>
2024-09-17 21:54:26 -0300

    Reformulação nos parâmetros e na resposta da API

--- app/controllers/book_recommendations_controller.rb
+++ app/controllers/book_recommendations_controller.rb
@@ -1,29 +1,8 @@
 class BookRecommendationsController < ApplicationController
 
   def generate
-    user = user_params
-    books = books_params
-
-    recommendations = BookRecommender.call(user: user, books: books)
+    recommendations = BookRecommender.call(genres: params[:genres])
 
     render json: recommendations
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :age, preferences: [])
-  end
-
-  def books_params
-    params.require(:books).map do |book|
-      {
-        id: book[:id],
-        title: book[:title],
-        authors: book[:authors],
-        genres: book[:genres],
-        description: book[:description]
-      }
-    end
   end
 end
--- app/services/book_recommender.rb
+++ app/services/book_recommender.rb
@@ -1,52 +1,43 @@
 class BookRecommender
-  def self.call(user:, books:)
-    prompt = generate_prompt(user, books)
-    response = ChatgptClient.call(prompt)
-    parse_response(response, books)
+  def self.call(genres:)
+    prompt = generate_prompt(genres)
+    ChatgptClient.call(prompt)
   end
 
   private
 
-  def self.generate_prompt(user, books)
-    user_info = "User info: #{user[:name]}, Age: #{user[:age]}, Preferences: #{user[:preferences].join(', ')}."
+  def self.generate_prompt(genres)
+    info = "Genres: #{genres.join(', ')}."
     
-    books_info = books.map do |book|
-      "Book ID: #{book[:id]}, Title: '#{book[:title]}', Authors: #{book[:authors].join(', ')}, Genres: #{book[:genres].join(', ')}, Description: #{book[:description]}."
-    end.join("\n")
-
     <<~PROMPT
-      Based on the user information:
-      #{user_info}
+      Based on these genres:
+      #{info}
+      
+      Please list exactly 5 different books from any book available worldwide that fit within these genres. You should include a diverse range of books, not limited to the most popular or previously recommended ones. 
 
-      And the following list of books:
-      #{books_info}
-
-      Please select exactly 3 books from the list that would be the best fit for the user. For each selected book, provide a brief explanation of why it was chosen for this user. Make sure to choose exactly 3 books.
+      **Make sure to provide new recommendations that have not been suggested before**.
 
       Your response should be in the following format:
-      - Book ID: <book_id>
-      - Recommendation: <reason for recommendation>
+    
+      {
+        "recommendations": [
+          {
+            "name": "<book title>"
+          },
+          {
+            "name": "<book title>"
+          },
+          {
+            "name": "<book title>"
+          },
+          {
+            "name": "<book title>"
+          },
+          {
+            "name": "<book title>"
+          }
+        ]
+      }
     PROMPT
-  end
-
-  def self.parse_response(response, books)
-    # Divide a resposta por linhas e extraia o ID do livro e a recomendação associada
-    lines = response.split("\n").reject(&:empty?)
-    recommendations = []
-
-    lines.each_slice(2) do |book_id_line, recommendation_line|
-      if book_id_line&.start_with?("- Book ID:")
-        book_id = book_id_line.split(":").last.strip.to_i
-        recommendation = recommendation_line&.split(":")&.last&.strip || "No recommendation found."
-        recommendations << { id: book_id, recommendation: recommendation }
-      end
-    end
-
-    # Garantir que o número de recomendações seja exatamente 3
-    if recommendations.size < 3
-      recommendations += Array.new(3 - recommendations.size) { |i| { id: nil, recommendation: "No additional recommendations found." } }
-    end
-
-    { recommendations: recommendations }
-  end
+  end  
 end
